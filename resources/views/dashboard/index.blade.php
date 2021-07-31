717f0c17953ad7fb0bc05dac3f5272325d4230c3
arm <moungnoi_k@silpakorn.edu>
2021-07-31 08:11:33 +0700

    dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -12,18 +12,21 @@
     <!-- DataTales Example -->
     <div class="card shadow mb-4 dashboard-page">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">{{clean( trans('niva-backend.dashboard') , array('Attr.EnableID' => true))}}</h6>
+            <h6 class="m-0 font-weight-bold text-primary">{{clean( trans('niva-backend.dashboard') ,
+                array('Attr.EnableID' => true))}}</h6>
         </div>
         <div class="card-body">
-        <div class="row">
+            <div class="row">
 
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-6 col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean( trans('niva-backend.blog') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$post_number}} {{clean( trans('niva-backend.posts') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean(
+                                        trans('niva-backend.blog') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$post_number}} {{clean(
+                                        trans('niva-backend.posts') , array('Attr.EnableID' => true))}}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -33,29 +36,15 @@
                     </div>
                 </div>
 
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-6 col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean( trans('niva-backend.projects') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$project_number}} {{clean( trans('niva-backend.projects') , array('Attr.EnableID' => true))}}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-4 col-md-6 mb-4">
-                    <div class="card border-left-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean( trans('niva-backend.what_we_do') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$service_number}} {{clean( trans('niva-backend.services') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean(
+                                        trans('niva-backend.projects') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$project_number}} {{clean(
+                                        trans('niva-backend.projects') , array('Attr.EnableID' => true))}}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -67,29 +56,15 @@
 
 
 
-                <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-6 col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean( trans('niva-backend.feedback') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$testimonial_number}} {{clean( trans('niva-backend.testimonials') , array('Attr.EnableID' => true))}}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                 <div class="col-xl-4 col-md-6 mb-4">
-                    <div class="card border-left-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> {{clean( trans('niva-backend.our_team') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$member_number}} {{clean( trans('niva-backend.members') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{clean(
+                                        trans('niva-backend.feedback') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$testimonial_number}} {{clean(
+                                        trans('niva-backend.testimonials') , array('Attr.EnableID' => true))}}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -100,13 +75,15 @@
                 </div>
 
 
-                 <div class="col-xl-4 col-md-6 mb-4">
+                <div class="col-xl-6 col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> {{clean( trans('niva-backend.our_clients') , array('Attr.EnableID' => true))}}</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$client_number}} {{clean( trans('niva-backend.clients') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> {{clean(
+                                        trans('niva-backend.our_clients') , array('Attr.EnableID' => true))}}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$client_number}} {{clean(
+                                        trans('niva-backend.clients') , array('Attr.EnableID' => true))}}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -117,7 +94,7 @@
                 </div>
 
 
-        </div>
+            </div>
         </div>
 
     </div>
@@ -125,5 +102,3 @@
 <!-- /.container-fluid -->
 
 @stop
-
-
